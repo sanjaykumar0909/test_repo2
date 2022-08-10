ec65f16f5dda1684448fc52a4ebe7e21eca9edc5
sanjaykumar0909 <alpha.sanjayj@gmail.com>
2022-08-10 19:07:16 +0530

    undone

---
+++ Ccode1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int c;
+	scanf("%c", &c);
+	switch c{
+		case 1:
+		power(a,b);
+		break;
+		case 2:
+		exponential(a,b);
+		break;
+		default:
+		printf("invalid input");
+	}
+}
+void power(float n1, float n2){
+	float c=pow(n1, n2);
+	printf("%f",c);
+}
+void exponential(float n1){
+	float b=exp(n1);
+	printf("%f",b);
+}
+void sqroot(float n1){
+    float b=sqrt(n1);
+    printf("%f", b);
+}
+void fun(){
+  printf("power, exponential");
+}
